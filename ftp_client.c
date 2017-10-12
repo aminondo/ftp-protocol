0a721c5307bab8bbeebd58fb547f97e0db7df4a5
Antonio Minondo <aminondo@nd.edu>
2017-10-12 00:07:11 -0400

    finished list command

--- ftp_client.c
+++ ftp_client.c
@@ -16,21 +16,21 @@ int main( int argc, char * argv[] ) {
   struct hostent *hp;
   struct sockaddr_in sin;
   char *host;
-  char buff[MAXLINE];
+  char buff[MAXLINE], msg[MAXLINE];
   int len, s;
 
   //check arguments
   if(argc == 2){
     host = argv[1];
   } else {
-    fprintf(stderr, "ERROR: error in arguments provided");
+    fprintf(stderr, "ERROR: error in arguments provided\n");
     exit(1);
   }
 
   //translate host name into peer's ip address
   hp = gethostbyname(host);
   if(!hp) {
-    fprintf(stderr, "ERROR: unknown host");
+    fprintf(stderr, "ERROR: unknown host\n");
     exit(1);
   }
 
@@ -42,7 +42,7 @@ int main( int argc, char * argv[] ) {
 
   //create socket
   if ( (s = socket(PF_INET, SOCK_STREAM, 0)) < 0 ){
-    perror("Error creating socket");
+    perror("Error creating socket\n");
     exit(1);
   }
   //welcome message
@@ -60,22 +60,12 @@ int main( int argc, char * argv[] ) {
 
   //connect created socket to remote server
   if(connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0){
-    perror("Error connnecting socket");
+    perror("Error connnecting socket\n");
     exit(1);
   }
 
   //main loop
   while(fgets(buff, sizeof(buff), stdin)) {
-    printf("DWLD: download a file from server\n");
-    printf("UPLD: upload a file to the server\n");
-    printf("DELF: delete file from server\n");
-    printf("LIST: list the directory on the server\n");
-    printf("MDIR: create a directory on the server\n");
-    printf("RDIR: remove a directory from the server\n");
-    printf("CDIR: change to a different directory on the server\n");
-    printf("QUIT: stop application\n");
-    printf("------------------------------------------------------------\n\n");
-    printf(">> ");
     buff[MAXLINE-1] = '\0';
 
     if(!strncmp(buff, "QUIT", 4)){ //quit command
@@ -87,14 +77,42 @@ int main( int argc, char * argv[] ) {
         perror("Client send error\n");
         exit(1);
       }
+      if((len = recv(s, msg, sizeof(msg), 0)) == -1){
+        perror("Client receive error\n");
+        exit(1);
+      }
+      printf("%s\n", msg);
     } else if(!strncmp(buff, "DWLD", 4)){ //dwld command
       len = strlen(buff) + 1;
       if(send(s, buff, len, 0) == -1){
         perror("Client send error\n");
         exit(1);
       }
-    }
+    } else if(!strncmp(buff, "CDIR", 4)){ //cdir command
+      if(send(s, buff, len, 0) == -1){
+        perror("Client send error\n");
+        exit(1);
+      }
+      printf("new path: ");
+      fgets(buff, sizeof(buff), stdin);
+      len = strlen(buff) + 1;
+      if(send(s, buff, len, 0) == -1){
+        perror("Client send error\n");
+        exit(1);
+      }
 
+    }
+    printf("\n------------------------------------------------------------\n");
+    printf("DWLD: download a file from server\n");
+    printf("UPLD: upload a file to the server\n");
+    printf("DELF: delete file from server\n");
+    printf("LIST: list the directory on the server\n");
+    printf("MDIR: create a directory on the server\n");
+    printf("RDIR: remove a directory from the server\n");
+    printf("CDIR: change to a different directory on the server\n");
+    printf("QUIT: stop application\n");
+    printf("------------------------------------------------------------\n\n");
+    printf(">> ");
 
   }
 
--- ftp_server.c
+++ ftp_server.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <dirent.h>
 #include <unistd.h>
 
 #define PORT 41046
@@ -14,7 +16,7 @@
 
 int main() {
   struct sockaddr_in sin;
-  char buff[MAXLINE];
+  char buff[MAXLINE], msg[MAXLINE];
   int len, s, new_s, opt;
 
   //build address data structure
@@ -63,6 +65,55 @@ int main() {
       if(len==0)
         break;
       printf("TCP recieved: %s\n", buff);
+
+      // Program gets directory listing and sends back to client
+      if(!strncmp(buff, "LIST", 4)){
+        struct dirent * dir;
+        struct stat fileStat;
+        DIR * d = opendir(".");
+        if(d == NULL){
+          perror("ERROR: error reading directory");
+        }
+        //loops through all directory items
+        while((dir = readdir(d)) != NULL){
+          //gets all file permissions
+          if(!stat(dir->d_name, &fileStat)){
+            strcat(msg,(S_ISDIR(fileStat.st_mode))  ? "d" : "-");
+            strcat(msg,(fileStat.st_mode & S_IRUSR) ? "r" : "-");
+            strcat(msg,(fileStat.st_mode & S_IWUSR) ? "w" : "-");
+            strcat(msg,(fileStat.st_mode & S_IXUSR) ? "x" : "-");
+            strcat(msg,(fileStat.st_mode & S_IRGRP) ? "r" : "-");
+            strcat(msg,(fileStat.st_mode & S_IWGRP) ? "w" : "-");
+            strcat(msg,(fileStat.st_mode & S_IXGRP) ? "x" : "-");
+            strcat(msg,(fileStat.st_mode & S_IROTH) ? "r" : "-");
+            strcat(msg,(fileStat.st_mode & S_IWOTH) ? "w" : "-");
+            strcat(msg,(fileStat.st_mode & S_IXOTH) ? "x" : "-");
+          } else {
+            perror("Error in stat");
+          }
+          //attach file name
+          strcat(msg, " ");
+          strcat(msg, dir->d_name);
+          strcat(msg, "\n");
+        }
+        //sends back to client
+        if(send(new_s, msg, strlen(msg), 0) == -1){
+          perror("Server send error\n");
+          exit(1);
+        }
+        closedir(d);
+      }
+      //changes active directory
+      if(!strncmp(buff, "CDIR", 4)){
+        //waiting for message from client
+        if((len = recv(new_s, buff, sizeof(buff), 0)) == -1){
+          perror("Server recieve error");
+          exit(1);
+        }
+        printf("changing directory to %s\n", buff);
+        if(chdir(buff) != 0)
+          perror("Error changing directory");
+      }
     }
 
     //close connection
